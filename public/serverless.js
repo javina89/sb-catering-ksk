f7ad08eeb71c030e7f838c25c80ec012c3745e6a
javina89 <mail@jesusavina.com>
2024-03-02 18:04:00 -0600

    converted code for Netlify5

--- public/serverless.js
+++ public/serverless.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function fetchCategories() {
-    console.log(process.env.TEST);
     const url = '/.netlify/functions/fetch-categories';
 
     fetch(url)
